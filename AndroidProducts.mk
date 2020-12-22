f842ec83c4fe724842b23e4ca63a4d2e48fc5568
Raj Sharma <rajmwr5785@gmail.com>
2020-12-22 14:49:12 +0000

    Bring up arrow

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rolex.mk
+    $(LOCAL_DIR)/arrow_rolex.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rolex-user \
-    lineage_rolex-userdebug
+    arrow_rolex-user \
+    arrow_rolex-userdebug
